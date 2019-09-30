5552bb65cc69d101ec4f3745220c25f7fb2926f0
homeant <homeanter@163.com>
2019-09-30 22:09:38 +0800

    Integration 请求github示例

--- vyse-integration/src/test/java/fun/vyse/cloud/test/ApplicationTest.java
+++ vyse-integration/src/test/java/fun/vyse/cloud/test/ApplicationTest.java
@@ -1,5 +1,6 @@
 package fun.vyse.cloud.test;
 
+import fun.vyse.cloud.test.entity.UserInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,10 +9,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.handler.LoggingHandler;
+import org.springframework.integration.http.dsl.Http;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -24,11 +31,15 @@ public class ApplicationTest {
     @Test
     public void test() {
         final String tom = tempConverter.hello("tom");
-        log.debug("message:{}",tom);
+        log.debug("message:{}", tom);
+        log.debug("userInfo:{}",tempConverter.userInfo("homeant"));
     }
 
 
+
     @Configuration
+    @EnableIntegration
+    @IntegrationComponentScan
     @EnableAutoConfiguration
     public static class Config {
         @MessagingGateway
@@ -37,11 +48,22 @@ public class ApplicationTest {
             @Gateway(requestChannel = "convert.input")
             String hello(String name);
 
+            @Gateway(requestChannel = "userInfo.input")
+            UserInfo userInfo(String name);
+
         }
 
         @Bean
         public IntegrationFlow convert() {
-            return f -> f.transform(r -> "hello " + r);
+            return f->f.handle(Http.outboundGateway("https://api.github.com/users/homeant")
+                    .httpMethod(HttpMethod.GET).expectedResponseType(String.class)).log(LoggingHandler.Level.DEBUG).bridge();
         }
+
+		@Bean
+        public IntegrationFlow userInfo(){
+        	return f->f.handle(Http.outboundGateway("https://api.github.com/users/{name}").uriVariable("name",r->r.getPayload())
+					.httpMethod(HttpMethod.GET).expectedResponseType(UserInfo.class))
+					.log(LoggingHandler.Level.DEBUG).bridge();
+		}
     }
 }
---
+++ vyse-integration/src/test/java/fun/vyse/cloud/test/entity/UserInfo.java
@@ -0,0 +1,41 @@
+package fun.vyse.cloud.test.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class UserInfo {
+
+	private String login;
+	private int id;
+	private String node_id;
+	private String avatar_url;
+	private String gravatar_id;
+	private String url;
+	private String html_url;
+	private String followers_url;
+	private String following_url;
+	private String gists_url;
+	private String starred_url;
+	private String subscriptions_url;
+	private String organizations_url;
+	private String repos_url;
+	private String events_url;
+	private String received_events_url;
+	private String type;
+	private boolean site_admin;
+	private String name;
+	private Object company;
+	private String blog;
+	private String location;
+	private Object email;
+	private Object hireable;
+	private String bio;
+	private int public_repos;
+	private int public_gists;
+	private int followers;
+	private int following;
+	private String created_at;
+	private String updated_at;
+}
