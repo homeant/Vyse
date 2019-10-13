1387f04cb2f71d7e8c3b8cd3b50d615684030db0
homeant <homeanter@163.com>
2019-10-13 10:34:05 +0800

    基础实体提交

---
+++ Vyse-core/src/main/java/fun/vyse/cloud/core/domain/IEntity.java
@@ -0,0 +1,11 @@
+package fun.vyse.cloud.core.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author junchen
+ * @Description 接口类
+ * @date 2019-10-13 09:48
+ */
+public interface IEntity extends Serializable,Cloneable {
+}
