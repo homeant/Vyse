0a05d9854cea55be070f42011589c3000b4fb82d
homeant <homeanter@163.com>
2019-11-04 20:37:31 +0800

    调整实体

---
+++ vyse-define/src/main/java/fun/vyse/cloud/define/repository/IConnectionActRepository.java
@@ -0,0 +1,14 @@
+package fun.vyse.cloud.define.repository;
+
+import fun.vyse.cloud.core.repository.IBaseRepository;
+import fun.vyse.cloud.define.entity.actual.ConnectionActEO;
+
+/**
+ * IConnectionActRepository
+ *
+ * @author junchen homeanter@163.com
+ * @date 2019-11-04 20:14
+ */
+public interface IConnectionActRepository extends IBaseRepository<ConnectionActEO,Long> {
+
+}
